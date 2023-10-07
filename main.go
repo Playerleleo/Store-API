10a6c0098cf41a89f8b745562914faf957867b2b
Playerleleo <leonardogomesdossantos123@gmail.com>
2023-10-07 14:38:55 -0300

    fix front

--- main.go
+++ main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"net/http"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price      float64
+	Quantity    int
+}
 
 var templateHtml = template.Must(template.ParseGlob("front/*.html"))
 
 func main() {
-	http.HandleFunc("/", Index)
-	http.ListenAndServe(":8080",nil)
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8080", nil)
 }
 
-func Index(w http.ResponseWriter, r *http.Request) {
-templateHtml.ExecuteTemplate(w, "Index", nil)
+func index(w http.ResponseWriter, r *http.Request) {
+    produtos := []Product{
+        {Name: "Camiseta", Description: "Azul, bem bonita", Price: 39, Quantity: 5},
+    }
+
+    templateHtml.ExecuteTemplate(w, "Index", produtos)
 }
